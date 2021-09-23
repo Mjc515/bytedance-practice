7ecc79dc25f2fcb4f1d7f3ba4caa7d729523bf59
Mjc515 <532349099@qq.com>
2021-09-23 22:46:58 +0800

    react-app代码提交

--- Day03/react-app/src/App.js
+++ Day03/react-app/src/App.js
@@ -1,48 +1,23 @@
-import { Button, Input, Menu } from 'antd';
-import { Row, Col} from 'antd';
-import 'antd/dist/antd.css';
+import logo from './logo.svg';
 import './App.css';
-import { UnorderedListOutlined } from '@ant-design/icons';
-import { BigLogo } from "./BigLogo.js";
-
-
 
 function App() {
   return (
     <div className="App">
-      <Row>
-        <Col>
-          <h1><a id="logo"><img alt="logo" src="logo.svg"/>Ant Design</a></h1>
-        </Col>
-        <Col flex="1">
-          <div id="searchbox">
-            <Input placeholder="搜索" />
-          </div>
-        </Col>
-        <Col>
-          <Menu id="menu" mode="horizontal">
-            <Menu.Item>设计</Menu.Item>
-            <Menu.Item>文档</Menu.Item>
-            <Menu.Item>组件</Menu.Item>
-            <Menu.Item>资源</Menu.Item>
-            <Menu.Item>国内镜像</Menu.Item>
-            <Menu.SubMenu title=" " icon={<UnorderedListOutlined />}>
-              <Menu.Item>子菜单项</Menu.Item>
-            </Menu.SubMenu>
-          </Menu>
-        </Col>
-      </Row>
-      <Row justify="center">
-        <BigLogo />
-        
-      </Row>
-      <Row justify="center">
-        <div id="btns">
-          <Button type="primary" shape="round">开始使用</Button>
-          <Button type="secondary" shape="round">设计语言</Button>
-        </div>
-      </Row>
-      
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
